157e865054c88c2e4a1d6ab81ef86b0cf4f6aa1d
Gabriel5934 <gabriel.andrade@outlook.com.br>
2026-01-02 12:12:51 -0300

    login flow coupled to fullstory recording

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import { FullStory, init } from "@fullstory/browser";
 
 function App() {
-  const [displayName, setDisplayName] = useState("");
+  const [displayName, setDisplayName] = useState<string | null>(
+    localStorage.getItem("displayName") ?? null
+  );
+  const [displayNameInput, setDisplayNameInput] = useState("");
+  const isInitialized = useRef(false);
 
   const isEnabled = import.meta.env.VITE_FULLSTORY_ENABLED === "true";
 
   useEffect(() => {
-    if (isEnabled) {
+    if (isEnabled && displayName && !isInitialized.current) {
       init({
         orgId: import.meta.env.VITE_FULLSTORY_ORG_ID,
-        devMode: false, //import.meta.env.DEV,
+        devMode: import.meta.env.DEV,
       });
+      isInitialized.current = true;
     }
-  }, [isEnabled]);
+  }, [isEnabled, displayName]);
 
   const identify = () => {
+    if (!displayName) return;
+
     FullStory("setIdentity", {
       uid: displayName.replace(/ /g, "").toLowerCase(),
       properties: {
@@ -26,15 +33,47 @@ function App() {
     });
   };
 
+  const login = () => {
+    localStorage.setItem("displayName", displayNameInput);
+    setDisplayName(displayNameInput);
+  };
+
+  const initSession = () => {
+    login();
+    identify();
+  };
+
+  const logout = () => {
+    localStorage.removeItem("displayName");
+    setDisplayName(null);
+    setDisplayNameInput("");
+    FullStory("shutdown");
+  };
+
   return (
-    <>
-      <input
-        type="text"
-        value={displayName}
-        onChange={(e) => setDisplayName(e.target.value)}
-      />
-      <button onClick={identify}>Identify</button>
-    </>
+    <div style={{ display: "flex", flexDirection: "column", gap: "10px" }}>
+      <h3 style={{ color: "white" }}>{`${
+        displayName ? `Logged in as ${displayName}` : "Log in"
+      }`}</h3>
+      {!displayName && (
+        <input
+          type="text"
+          value={displayNameInput}
+          onChange={(e) => setDisplayNameInput(e.target.value)}
+        />
+      )}
+      {displayName ? (
+        <button onClick={logout}>Log out</button>
+      ) : (
+        <button onClick={initSession}>Log in</button>
+      )}
+      <button
+        id="big-orange-button"
+        style={{ backgroundColor: "orange", color: "white" }}
+      >
+        Big Orange Button
+      </button>
+    </div>
   );
 }
 
